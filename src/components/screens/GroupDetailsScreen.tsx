e6bbdcfae2ae5b51a410da7ad07b46a33d645c64
Tao Bror Bojlén <tao@btao.org>
2019-02-27 20:14:48 +0000

    redesign group details page

--- src/components/screens/GroupDetailsScreen.tsx
+++ src/components/screens/GroupDetailsScreen.tsx
@@ -2,8 +2,8 @@ import hoistNonReactStatics from "hoist-non-react-statics";
 import { inflect } from "inflection";
 import { remove } from "lodash";
 import * as React from "react";
-import { Alert, ScrollView, StyleSheet } from "react-native";
-import { Image, ListItem } from "react-native-elements";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+import { Image } from "react-native-elements";
 import { NavigationInjectedProps } from "react-navigation";
 
 import { getGroups, getGroups_groups } from "../../generated/getGroups";
@@ -21,7 +21,8 @@ import {
 import { GROUP_DETAILS_QUERY, GroupDetailsQuery, GroupDetailsQueryResult, GROUPS_QUERY } from "../../graphql/queries";
 import { NavigationService } from "../../services";
 import { colors } from "../../style/common";
-import { ListSection } from "../atoms";
+import { ListSection, WobblyButton } from "../atoms";
+import { Intent } from "../atoms/WobblyButton";
 import WobblyText from "../atoms/WobblyText";
 import { LoadingState, PersonList } from "../organisms";
 
@@ -58,24 +59,18 @@ class GroupDetailsScreen extends React.PureComponent<IGroupDetailsScreen> {
       const members = group.members || [];
       return (
         <ScrollView style={style.container}>
-          <Image source={{ uri: "https://placeimg.com/600/200/nature" }} resizeMode="cover" style={style.groupImage} />
-          <ListSection>
-            <ListItem title={<WobblyText headline={true}>{this.groupName}</WobblyText>} bottomDivider={true} />
-            <ListItem title={group.description || "Add a description"} bottomDivider={true} />
-          </ListSection>
+          <Image source={{ uri: "https://placeimg.com/600/200/nature" }} style={style.groupImage} />
+          <WobblyText title1={true}>{this.groupName}</WobblyText>
+          <View style={style.description}>
+            <WobblyText>{group.description || "Add a description"}</WobblyText>
+          </View>
           <ListSection>
             <WobblyText listHeading={true}>
               {`${members.length} ${inflect("member", members.length)}`.toUpperCase()}
             </WobblyText>
             <PersonList people={members} />
           </ListSection>
-          <ListSection>
-            <ListItem
-              title="Leave group"
-              leftIcon={{ name: "md-exit", type: "ionicon" }}
-              onPress={this.handleLeaveGroup}
-            />
-          </ListSection>
+          <WobblyButton text="Leave group" intent={Intent.DANGER} onPress={this.handleLeaveGroup} />
         </ScrollView>
       );
     }
@@ -95,12 +90,22 @@ class GroupDetailsScreen extends React.PureComponent<IGroupDetailsScreen> {
 
 const style = StyleSheet.create({
   container: {
-    backgroundColor: colors.lightGray4,
-    height: "100%"
+    flex: 1,
+    padding: 10
   },
   groupImage: {
     height: 200,
-    width: "100%"
+    width: "100%",
+    resizeMode: "cover",
+    borderRadius: 10,
+    marginBottom: 10
+  },
+  description: {
+    marginTop: 10,
+    marginBottom: 20,
+    backgroundColor: colors.lightGray4,
+    padding: 10,
+    borderRadius: 10
   }
 });
 
